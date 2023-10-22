68ed5f99f7f20438772c060e61e54e57c8c16adf
grassnhi <nhi.nguyenn@hcmut.edu.vn>
2023-10-22 08:52:34 +0700

    feat(package): Add and update package.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
 
 import Routing from "./backend/route";
-
+import Printlocate from "./components/printlocate/Printlocate";
+import Uploadtable from "./utils/uploadtable";
+import ChoosePrinter from "./utils/choosePrinter";
+import Printproperties from "./utils/printproperties";
+import Cantbuy from "./components/announcement/cantbuy";
+import Successbuy from "./components/announcement/successbuy";
+import Home from "./components/home/Home";
+import Completeprint from "./components/announcement/completeprint";
 function App() {
   return (
     <>
-      <Routing />
+      <Printproperties />
     </>
   );
 }
