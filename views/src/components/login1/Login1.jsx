c5a0b1f191672c1fecdef58ce2ed668b641c8b23
Germanyy0410 <tienduchoang136@gmail.com>
2023-11-15 11:37:41 +0700

    feat: fix Home & Footer

--- views/src/components/login1/Login1.jsx
+++ views/src/components/login1/Login1.jsx
@@ -16,18 +16,6 @@ const Login1 = () => {
   const navigate = useNavigate();
   return (
     <div className="biggestcontainer">
-      <div className="wrapper">
-        <div className="copyright">Bản quyền © Thiếu Nhi-CC02</div>
-        <div className="footer-right">
-          <span className="phat-trien-boi-thieu-nhi-cc02">
-            <span>Phát triển bởi Thiếu Nhi-CC02</span>
-            <span className="span">{` | `}</span>
-          </span>
-          <span className="policy">Điều khoản & điều kiện</span>
-          <span className="span1">{` | `}</span>
-          <span className="policy">Chính sách pháp lý</span>
-        </div>
-      </div>
       <div className="header">
         <img className="bklogo" src={logo2} alt="bklogo" />
         <div className="name">BK Fast Automated Printing Service </div>
--- views/src/routes/route.jsx
+++ views/src/routes/route.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import Login from "../components/login/Login";
 import Home from "../components/home/Home";
 import Login1 from "../components/login1/Login1";
@@ -11,23 +11,22 @@ import Upload from "../components/upload/upload";
 import Adminusers from "../components/profile/adminusers";
 import Users from "../components/profile/users";
 import Printlocate from "../components/printlocate/Printlocate";
-import Help from "../components/help/Help"
+import Help from "../components/help/Help";
 const Routing = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="Choose" element={<Choose />}>
-        <Route path="Login1" element={<Login1 />}>
-          <Route path="Home" element={<Home />} />
-          <Route path="Upload" element={<Upload />} />
-          <Route path="Completeprint" element={<Completeprint />} />
-          <Route path="Profile" element={<Users />} />
-          <Route path="PrintLocate" element={<Printlocate />} />
-        </Route>
-        <Route path="AdminLogin1" element={<AdminLogin1 />}>
-          <Route path="AdminHome" element={<Adminusers />} />
-        </Route>
-      </Route>
+      <Route path="Choose" element={<Choose />} />
+      <Route path="Choose/Login1" element={<Login1 />} />
+      <Route path="Choose/Login1/Home" element={<Home />} />
+      <Route path="Choose/Login1/Upload" element={<Upload />} />
+      <Route path="Choose/Login1/Completeprint" element={<Completeprint />} />
+      <Route path="Choose/Login1/Profile" element={<Users />} />
+      <Route path="Choose/Login1/PrintLocate" element={<Printlocate />} />
+      <Route path="AdminLogin1" element={<AdminLogin1 />} />
+      <Route path="AdminLogin1/AdminHome" element={<AdminHome />} />
+      <Route path="AdminLogin1/AdminHome/AdminUsers" element={<Adminusers />} />
+      
       <Route path="help" element={<Help />} />
     </Routes>
   );
